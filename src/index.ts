eb9d24e3e0c42e2bc4b98e95cecacff761b3c963
Ayan <ayan2572006@gmail.com>
2024-09-01 01:55:22 +0530

    body header param get

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.json({
-    message: "Hello Hono!",
-  });
-});
-app.post("/users", (c) => {
+app.post("/", async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
   return c.text("Hello Hono!");
 });
 
